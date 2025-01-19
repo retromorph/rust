3da70e21b200bc7a851eaef95f2fab2c3ab38a77
Nikita Musalov <musalov@eesee.io>
2025-01-20 00:03:00 +0300

    feat: structs/trust

--- problems/structs/trust/src/lib.rs
+++ problems/structs/trust/src/lib.rs
@@ -3,6 +3,12 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum AgentOutcome {
+    Cooperate,
+    Cheat,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum RoundOutcome {
     BothCooperated,
     LeftCheated,
@@ -10,24 +16,21 @@ pub enum RoundOutcome {
     BothCheated,
 }
 
-pub trait Player {
-    fn cooperate(&self) -> bool;
-    fn other_s_turn(&mut self, turn: bool);
-}
+////////////////////////////////////////////////////////////////////////////////
 
 pub struct Game {
-    // TODO: your code goes here.
-    left: Box<dyn Player>,
-    right: Box<dyn Player>,
+    left_agent: Box<dyn Agent>,
+    right_agent: Box<dyn Agent>,
+
     left_score: i32,
     right_score: i32,
 }
 
 impl Game {
-    pub fn new(left: Box<dyn Player>, right: Box<dyn Player>) -> Self {
-        Game {
-            left,
-            right,
+    pub fn new(left_agent: Box<dyn Agent>, right_agent: Box<dyn Agent>) -> Self {
+        Self {
+            left_agent,
+            right_agent,
             left_score: 0,
             right_score: 0,
         }
@@ -42,41 +45,56 @@ impl Game {
     }
 
     pub fn play_round(&mut self) -> RoundOutcome {
-        let l = self.left.cooperate();
-        let r = self.right.cooperate();
-        self.left.other_s_turn(r);
-        self.right.other_s_turn(l);
+        let left_outcome = self.left_agent.play();
+        let right_outcome = self.right_agent.play();
 
-        if l && r {
-            self.left_score += 2;
-            self.right_score += 2;
-            RoundOutcome::BothCooperated
-        } else if l && !r {
-            self.right_score += 3;
-            self.left_score -= 1;
-            RoundOutcome::RightCheated
-        } else if !l && r {
-            self.left_score += 3;
-            self.right_score -= 1;
-            RoundOutcome::LeftCheated
-        } else {
-            RoundOutcome::BothCheated
+        self.left_agent.update(right_outcome);
+        self.right_agent.update(left_outcome);
+
+        match (left_outcome, right_outcome) {
+            (AgentOutcome::Cooperate, AgentOutcome::Cooperate) => {
+                self.left_score += 2;
+                self.right_score += 2;
+                RoundOutcome::BothCooperated
+            }
+            (AgentOutcome::Cooperate, AgentOutcome::Cheat) => {
+                self.left_score -= 1;
+                self.right_score += 3;
+                RoundOutcome::RightCheated
+            }
+            (AgentOutcome::Cheat, AgentOutcome::Cooperate) => {
+                self.left_score += 3;
+                self.right_score -= 1;
+                RoundOutcome::LeftCheated
+            }
+            (AgentOutcome::Cheat, AgentOutcome::Cheat) => {
+                self.left_score += 0;
+                self.right_score += 0;
+                RoundOutcome::BothCheated
+            }
         }
     }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
-#[derive(Default)]
-pub struct CheatingAgent {
-    // TODO: your code goes here.
+pub trait Agent {
+    fn play(&self) -> AgentOutcome;
+
+    fn update(&mut self, opponent_outcome: AgentOutcome);
 }
-impl Player for CheatingAgent {
-    fn cooperate(&self) -> bool {
-        false
+
+////////////////////////////////////////////////////////////////////////////////
+
+#[derive(Default)]
+pub struct CheatingAgent {}
+
+impl Agent for CheatingAgent {
+    fn play(&self) -> AgentOutcome {
+        AgentOutcome::Cheat
     }
 
-    fn other_s_turn(&mut self, _turn: bool) {}
+    fn update(&mut self, _: AgentOutcome) {}
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -84,91 +102,87 @@ impl Player for CheatingAgent {
 #[derive(Default)]
 pub struct CooperatingAgent {}
 
-impl Player for CooperatingAgent {
-    fn cooperate(&self) -> bool {
-        true
+impl Agent for CooperatingAgent {
+    fn play(&self) -> AgentOutcome {
+        AgentOutcome::Cooperate
     }
 
-    fn other_s_turn(&mut self, _turn: bool) {}
+    fn update(&mut self, _: AgentOutcome) {}
 }
+
 ////////////////////////////////////////////////////////////////////////////////
 
+#[derive(Default)]
 pub struct GrudgerAgent {
-    trust: bool,
-}
-impl Default for GrudgerAgent {
-    fn default() -> Self {
-        GrudgerAgent { trust: true }
-    }
-}
-impl Player for GrudgerAgent {
-    fn cooperate(&self) -> bool {
-        self.trust
-    }
-
-    fn other_s_turn(&mut self, turn: bool) {
-        if !turn {
-            self.trust = false;
-        }
-    }
+    is_enraged: bool,
 }
 
-////////////////////////////////////////////////////////////////////////////////
-
-pub struct CopycatAgent {
-    trust: bool,
-}
-impl Default for CopycatAgent {
-    fn default() -> Self {
-        CopycatAgent { trust: true }
-    }
-}
-impl Player for CopycatAgent {
-    fn cooperate(&self) -> bool {
-        self.trust
-    }
-
-    fn other_s_turn(&mut self, turn: bool) {
-        self.trust = turn;
-    }
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
-pub struct DetectiveAgent {
-    trust: bool,
-    num: i32,
-    other_cheated: bool,
-}
-
-impl Default for DetectiveAgent {
-    fn default() -> Self {
-        DetectiveAgent {
-            trust: true,
-            num: 0,
-            other_cheated: false,
-        }
-    }
-}
-
-impl Player for DetectiveAgent {
-    fn cooperate(&self) -> bool {
-        if self.num < 4 {
-            self.num != 1
-        } else if self.other_cheated {
-            self.trust
+impl Agent for GrudgerAgent {
+    fn play(&self) -> AgentOutcome {
+        if self.is_enraged {
+            AgentOutcome::Cheat
         } else {
-            false
+            AgentOutcome::Cooperate
         }
     }
 
-    fn other_s_turn(&mut self, turn: bool) {
-        if self.num >= 3 {
-            self.trust = turn;
-        } else if !turn {
-            self.other_cheated = true;
+    fn update(&mut self, opponent_outcome: AgentOutcome) {
+        if opponent_outcome == AgentOutcome::Cheat {
+            self.is_enraged = true;
         }
+    }
+}
 
-        self.num += 1;
+////////////////////////////////////////////////////////////////////////////////
+
+#[derive(Default)]
+pub struct CopycatAgent {
+    last_opponent_outcome: Option<AgentOutcome>,
+}
+
+impl Agent for CopycatAgent {
+    fn play(&self) -> AgentOutcome {
+        if self.last_opponent_outcome.is_none() {
+            AgentOutcome::Cooperate
+        } else {
+            self.last_opponent_outcome.unwrap()
+        }
+    }
+
+    fn update(&mut self, opponent_outcome: AgentOutcome) {
+        self.last_opponent_outcome = Some(opponent_outcome);
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+#[derive(Default)]
+pub struct DetectiveAgent {
+    round_number: u32,
+    is_opponent_cheated: bool,
+    last_opponent_outcome: Option<AgentOutcome>,
+}
+
+impl Agent for DetectiveAgent {
+    fn play(&self) -> AgentOutcome {
+        if self.round_number == 0 {
+            AgentOutcome::Cooperate
+        } else if self.round_number == 1 {
+            AgentOutcome::Cheat
+        } else if self.round_number == 2 || self.round_number == 3 {
+            AgentOutcome::Cooperate
+        } else if self.is_opponent_cheated {
+            self.last_opponent_outcome.unwrap()
+        } else {
+            AgentOutcome::Cheat
+        }
+    }
+
+    fn update(&mut self, opponent_outcome: AgentOutcome) {
+        if opponent_outcome == AgentOutcome::Cheat {
+            self.is_opponent_cheated = true;
+        }
+        self.last_opponent_outcome = Some(opponent_outcome);
+        self.round_number += 1;
     }
 }
