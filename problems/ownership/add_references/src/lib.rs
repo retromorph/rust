685bd37086d0706532f78276dff343e9130ae149
Nikita Musalov <musalov@eesee.io>
2025-01-19 03:56:59 +0300

    feat: ownership

--- problems/ownership/add_references/src/lib.rs
+++ problems/ownership/add_references/src/lib.rs
@@ -15,15 +15,17 @@ fn calculate_average(data: &Vec<i32>) -> i32 {
         sum += x;
         cnt += 1;
     }
+
     sum / cnt
 }
 
-fn calculate_median(data: &mut Vec<i32>) -> i32 {
+fn calculate_median(data: &mut [i32]) -> i32 {
     data.sort();
+
     data[data.len() / 2]
 }
 
-fn calculate_minmax(data: &[i32]) -> (i32, i32) {
+fn calculate_minmax(data: &Vec<i32>) -> (i32, i32) {
     let mut min = data[0];
     let mut max = data[0];
     for x in data {
@@ -34,14 +36,14 @@ fn calculate_minmax(data: &[i32]) -> (i32, i32) {
             max = *x;
         }
     }
+
     (min, max)
 }
 
 pub fn calculate_statistics(data: &Vec<i32>) -> Statistics {
     let average = calculate_average(data);
-    let mut data_med = data.clone();
-    let median = calculate_median(&mut data_med);
     let (min, max) = calculate_minmax(data);
+    let median = calculate_median(&mut data.clone());
 
     Statistics {
         average,
--- problems/ownership/kolhoz_hashmap/src/hashmap.rs
+++ problems/ownership/kolhoz_hashmap/src/hashmap.rs
@@ -3,83 +3,96 @@
 use crate::types::{Data, Key};
 
 pub fn new_hashmap(len: usize) -> Vec<Vec<(Key, Data)>> {
-    let mut hashmap = Vec::new();
-    for _i in 0..len {
-        let tmp: Vec<(Key, Data)> = Vec::new();
-        hashmap.push(tmp);
+    let mut table = Vec::new();
+    for _ in 0..len {
+        table.push(Vec::new());
     }
-    hashmap
+
+    table
 }
 
-pub fn insert(table: &mut Vec<Vec<(Key, Data)>>, key: Key, value: Data) -> &mut Data {
+pub fn insert(table: &mut [Vec<(Key, Data)>], key: Key, value: Data) -> &mut Data {
     if table.is_empty() {
         panic!("insert in empty kolhoz-table");
     }
-    let i = key.get_hash() as usize % table.len();
-    table[i].push((key, value));
 
-    return &mut table[i].last_mut().unwrap().1;
+    let bucket_index = (key.get_hash() as usize) % table.len();
+    table[bucket_index].push((key, value));
+
+    &mut table[bucket_index].last_mut().unwrap().1
 }
 
 pub fn get_one_or_default<'a>(
-    table: &'a Vec<Vec<(Key, Data)>>,
+    table: &'a [Vec<(Key, Data)>],
     key: &Key,
     default_value: &'a Data,
 ) -> &'a Data {
     if table.is_empty() {
         return default_value;
     }
-    let i = key.get_hash() as usize % table.len();
-    for el in &table[i] {
+
+    let bucket_index = (key.get_hash() as usize) % table.len();
+    let bucket = &table[bucket_index];
+
+    for el in bucket {
         if el.0 == *key {
             return &el.1;
         }
     }
+
     default_value
 }
 
 pub fn multi_get<'a, 'b>(
-    table: &'a Vec<Vec<(Key, Data)>>,
+    table: &'a [Vec<(Key, Data)>],
     keys: &'b Vec<Key>,
 ) -> Vec<(&'b Key, Vec<&'a Data>)> {
-    let mut ans: Vec<(&Key, Vec<&Data>)> = Vec::new();
+    let mut result: Vec<(&Key, Vec<&Data>)> = Vec::new();
+
     for key in keys {
-        let mut tmp: Vec<&Data> = Vec::new();
+        let mut per_key_result: Vec<&Data> = Vec::new();
         if table.is_empty() {
-            ans.push((key, tmp));
+            result.push((key, per_key_result));
             continue;
         }
+
         let i = key.get_hash() as usize % table.len();
         for el in &table[i] {
             if el.0 == *key {
-                tmp.push(&el.1);
+                per_key_result.push(&el.1);
             }
         }
-        ans.push((key, tmp));
+
+        result.push((key, per_key_result));
     }
-    ans
+
+    result
 }
 
-pub fn find_keys_of_data<'a>(table: &'a Vec<Vec<(Key, Data)>>, value: &Data) -> Vec<&'a Key> {
-    let mut ans: Vec<&Key> = Vec::new();
+pub fn find_keys_of_data<'a>(table: &'a [Vec<(Key, Data)>], value: &Data) -> Vec<&'a Key> {
+    let mut result: Vec<&Key> = Vec::new();
+
     for bucket in table {
         for el in bucket {
             if el.1 == *value {
-                ans.push(&el.0)
+                result.push(&el.0);
             }
         }
     }
-    ans
+
+    result
 }
 
 pub fn resize(table: &mut Vec<Vec<(Key, Data)>>, new_len: usize) {
-    let mut tmp: Vec<Vec<(Key, Data)>> = new_hashmap(new_len);
+    let mut result: Vec<Vec<(Key, Data)>> = new_hashmap(new_len);
+
     for bucket in &mut *table {
         while let Some(el) = bucket.pop() {
             if new_len > 0 {
-                insert(&mut tmp, el.0, el.1);
+                insert(&mut result, el.0, el.1);
             }
         }
     }
-    *table = tmp;
+
+    *table = result;
 }
